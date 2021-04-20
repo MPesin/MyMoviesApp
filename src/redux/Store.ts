e90aa421a9b3f7cfe8c92871c2013dd0e7b49603
MPesin <mikelpesin@gmail.com>
2021-04-20 23:22:16 +0300

    adding empty basic redux files

---
+++ src/redux/Store.ts
@@ -0,0 +1,5 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+const middleware = applyMiddleware(thunk);
+export const store = createStore(rootReducer, middleware);
